568bc3528c8eadfb7862604717b4961c4a5246f5
Jhonata Escatolini <jescatolini@gmail.com>
2023-05-29 19:45:36 -0300

    feat: update final Server.js

--- src/server.js
+++ src/server.js
@@ -1,8 +1,27 @@
-const app = require('express') ();
+const express = require('express');
 
-app.get('/', (req, res) => {
-  res.status(202).send("Hello, world!");
-})
+const app = express();
+
+const routes = require('./routes');
+const AppError = require('./utils/AppError');
+
+app.use(express.json());
+app.use('/', routes)
+app.use((error, req, res, next) => {
+  if ( error instanceof AppError ) {
+    return res.status(error.statusCode).json({
+      status: "error",
+      message: error.message
+    })
+  }
+
+  console.error(error);
+
+  return res.status(500).json({
+    status: "error",
+    message: "Internal Server Error"
+  })
+});
 
 const PORT = 5000;
 app.listen(PORT, () => {console.log(`Listening on port ${PORT}`)});
