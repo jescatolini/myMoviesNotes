5806266bcf13c59d314e5c2a41bb92a85b510629
Jhonata Escatolini <jescatolini@gmail.com>
2023-05-29 19:39:32 -0300

    feat: update Knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,8 +1,16 @@
+const path = require('path');
+
 module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
       filename: path.resolve(__dirname, "src", "database", "database.db")
+    },
+    migrations: {
+      directory: path.resolve(__dirname, "src" ,"database", "migrations")
+    },
+    pool: {
+      afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb)
     },
     useNullAsDefault: true
   }
